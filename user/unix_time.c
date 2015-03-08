3c9375d96ba5b75ac846c450709279d4fa6ac5d5
st0ff3r <stoffer@skulp.net>
2015-03-08 17:49:57 +0100

    unix_time: - system_time

--- user/unix_time.c
+++ user/unix_time.c
@@ -11,12 +11,14 @@
 #include "unix_time.h"
 
 uint64 unix_time = 0;
-uint64 system_time = 0;
-uint64 system_time_delta;
+//uint64 system_time = 0;
+//uint64 system_time_delta;
+//uint64 unix_time_delta;
 bool unix_time_mutex = false;
 
 //uint64 
 static volatile os_timer_t overflow_timer;
+static volatile os_timer_t ntp_update_timer;
 
 ICACHE_FLASH_ATTR void overflow_timerfunc(void *arg) {
 	//INFO("overflow_timer\n\r");
@@ -24,26 +26,29 @@ ICACHE_FLASH_ATTR void overflow_timerfunc(void *arg) {
 		// do nothing
 	}
 	unix_time_mutex = true;			// set mutex
-	system_time++;
+	unix_time++;
 	unix_time_mutex = false;		// free mutex
 }
 
+ICACHE_FLASH_ATTR void ntp_update_timerfunc(void *arg) {
+	INFO("ntp_update_timer\n\r");
+	ntp_get_time();
+}
+
 ICACHE_FLASH_ATTR void init_unix_time(void) {
-	system_time_delta = system_get_time() / 1000000;;
+	//system_time_delta = system_get_time() / 1000000;
+	//INFO("init_unix_time: system_time_delta: %llu\n\r", system_time_delta);
 	ntp_get_time();
 	
-	//start timer
-    //Disarm timer
+	// start unix time second incrementer
     os_timer_disarm(&overflow_timer);
-
-    //Setup timer
     os_timer_setfn(&overflow_timer, (os_timer_func_t *)overflow_timerfunc, NULL);
-
-    //Arm the timer
-    //&sample_timer is the pointer
-    //1000 is the fire time in ms
-    //0 for once and 1 for repeating
     os_timer_arm(&overflow_timer, 1000, 1);		// every second
+
+	// start ntp update timer
+    os_timer_disarm(&ntp_update_timer);
+    os_timer_setfn(&ntp_update_timer, (os_timer_func_t *)ntp_update_timerfunc, NULL);
+    os_timer_arm(&ntp_update_timer, 10000, 1);		// every 10 second
 }
 
 ICACHE_FLASH_ATTR uint64 get_unix_time(void) {
@@ -54,24 +59,32 @@ ICACHE_FLASH_ATTR uint64 get_unix_time(void) {
 	}
 	
 	unix_time_mutex = true;			// set mutex
-	INFO("unix_time: %llu system_time: %llu\n\r", unix_time, system_time);
-	current_unix_time = unix_time + system_time - system_time_delta;
+	INFO("unix_time: %llu\n\r", unix_time);
+	current_unix_time = unix_time;
 	unix_time_mutex = false;		// free mutex
 
 	return current_unix_time;
 }
 
 ICACHE_FLASH_ATTR void set_unix_time(uint64 current_unix_time) {
+	//uint64 last_system_time_delta;
 	while (unix_time_mutex) {
 		// do nothing
 	}
 
 	unix_time_mutex = true;			// set mutex
-	INFO("current_unix_time: %llu\n\r", current_unix_time);
+	//INFO("current_unix_time: %llu\n\r", current_unix_time);
+	
+	//INFO("system_time_delta: %llu\n\r", system_time_delta);
+	//last_system_time_delta = system_time_delta;
+	//system_time_delta = (system_get_time() / 1000000) - system_time_delta;
+	//INFO("system_time_delta: %llu\n\r", system_time_delta);
+	
+	
 	unix_time = current_unix_time;
 	INFO("unix_time: %llu\n\r", unix_time);
-	system_time = system_get_time() / 1000000;
-	INFO("system_time: %llu\n\r", system_time);
+	//system_time = system_get_time() / 1000000;
+	//INFO("system_time: %llu\n\r", system_time);
 	unix_time_mutex = false;		// free mutex
 	
 }
