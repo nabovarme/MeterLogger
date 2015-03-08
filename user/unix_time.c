49f79c39f1e752591842e188542685d5ad11626b
st0ff3r <stoffer@skulp.net>
2015-03-08 06:06:56 +0100

    init system_time_delta in init_unix_time()

--- user/unix_time.c
+++ user/unix_time.c
@@ -12,7 +12,7 @@
 
 uint64 unix_time = 0;
 uint64 system_time = 0;
-uint64 system_time_boot;
+uint64 system_time_delta;
 bool unix_time_mutex = false;
 
 //uint64 
@@ -29,6 +29,7 @@ ICACHE_FLASH_ATTR void overflow_timerfunc(void *arg) {
 }
 
 ICACHE_FLASH_ATTR void init_unix_time(void) {
+	system_time_delta = system_get_time() / 1000000;;
 	ntp_get_time();
 	
 	//start timer
@@ -53,8 +54,8 @@ ICACHE_FLASH_ATTR uint64 get_unix_time(void) {
 	}
 	
 	unix_time_mutex = true;			// set mutex
-	INFO("unix_time: %llu system_time: %llu system_get_time(): %lu\n\r", unix_time, system_time, system_get_time() / 1000000);
-	current_unix_time = unix_time + system_time - system_time_boot;
+	INFO("unix_time: %llu system_time: %llu\n\r", unix_time, system_time);
+	current_unix_time = unix_time + system_time - system_time_delta;
 	unix_time_mutex = false;		// free mutex
 
 	return current_unix_time;
@@ -70,7 +71,6 @@ ICACHE_FLASH_ATTR void set_unix_time(uint64 current_unix_time) {
 	unix_time = current_unix_time;
 	INFO("unix_time: %llu\n\r", unix_time);
 	system_time = system_get_time() / 1000000;
-	system_time_boot = system_time;
 	INFO("system_time: %llu\n\r", system_time);
 	unix_time_mutex = false;		// free mutex
 	
