84214cc899d1d20022b77266023f70b60faba5de
st0ff3r <stoffer@skulp.net>
2015-09-19 00:37:42 +0200

    removed ac out and led support on esp-03 branch

--- user/led.h
+++
@@ -1,7 +0,0 @@
-ICACHE_FLASH_ATTR void led_init(void);
-ICACHE_FLASH_ATTR void led_on(void);
-ICACHE_FLASH_ATTR void led_off(void);
-ICACHE_FLASH_ATTR void led_pattern_a(void);
-ICACHE_FLASH_ATTR void led_pattern_b(void);
-ICACHE_FLASH_ATTR void led_stop_pattern(void);
-ICACHE_FLASH_ATTR void led_blinker_timer_func(void *arg);
