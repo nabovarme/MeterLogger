81e1cc7af72691423d8db30a2e09955a55b18b39
st0ff3r <stoffer@skulp.net>
2015-09-23 02:26:01 +0200

    localtime() test

--- user/cron.c
+++ user/cron.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include "unix_time.h"
 #include "cron.h"
 
@@ -73,8 +74,13 @@ ICACHE_FLASH_ATTR void clear_cron_jobs() {
 }
 
 ICACHE_FLASH_ATTR void minute_timer_func(void *arg) {
+	struct tm *dt;
+	time_t unix_time;
 #ifdef DEBUG
-	//os_printf("t: %s\n\r", sntp_get_real_time(get_unix_time()));
+	unix_time = get_unix_time() + (2 * 60 * 60);	// BUG here - time zone support needed
+//	tzset();
+	dt = localtime(&unix_time);
+	os_printf("%02d:%02d:%02d %d.%d.%d\r\n", dt->tm_hour, dt->tm_min, dt->tm_sec, dt->tm_mday, dt->tm_mon + 1, dt->tm_year + 1900);
 	os_printf("rtc: %u\n\r", get_unix_time());
 #endif
 	// convert current unix time to hour, minutes... suitable for comparing with crontab
