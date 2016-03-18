35dfa4a2e669b43928acc0f324ab8ffb5dbef4a7
st0ff3r <stoffer@skulp.net>
2016-03-19 00:16:46 +0100

    typo

--- user/user_main.c
+++ user/user_main.c
@@ -447,7 +447,7 @@ ICACHE_FLASH_ATTR void mqttDataCb(uint32_t *args, const char* topic, uint32_t to
 #else
 		reply_topic_l = os_sprintf(reply_topic, "/reset_reason/v1/%07u/%u", kmp_get_received_serial(), get_unix_time());
 #endif
-		reply_message_l = os_sprintf(reply_message, "heap=%d&", (rtc_info != NULL) ? rtc_info->reason : -1);
+		reply_message_l = os_sprintf(reply_message, "%d", (rtc_info != NULL) ? rtc_info->reason : -1);
 #ifdef DEBUG
 		system_print_meminfo();
 #endif
