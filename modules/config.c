7bb63ba397eacc03e60a86700376af6efd1de886
st0ff3r <stoffer@skulp.net>
2016-03-22 19:04:15 +0100

    misplaced #ifdef IMPULSE

--- modules/config.c
+++ modules/config.c
@@ -85,14 +85,14 @@ cfg_load() {
 
 		sys_cfg.mqtt_keepalive = MQTT_KEEPALIVE;
 		
-#ifdef IMPULSE
 		sys_cfg.ac_thermo_state = 0;
 		memset(&sys_cfg.cron_jobs, 0, sizeof(cron_job_t));
+#ifdef IMPULSE
 		os_sprintf(sys_cfg.impulse_meter_serial, DEFAULT_METER_SERIAL);
 		os_sprintf(sys_cfg.impulse_meter_energy, "0");
 		os_sprintf(sys_cfg.impulses_per_kwh, "100");
 		sys_cfg.impulse_meter_count = 0;
-#endif
+#endif // IMPULSE
 		INFO(" default configuration\r\n");
 
 		cfg_save();
