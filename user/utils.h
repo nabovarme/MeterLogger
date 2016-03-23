68f9b7cab792be02f3d1c5d03a59d2325c96d1be
st0ff3r <stoffer@skulp.net>
2016-03-23 20:51:06 +0100

    + added to utils.c/utils.h but not used: 	void float_to_string(float value, char *value_string, int8_t max_decimals); 	float string_to_float(unsigned char *value_string, int8_t max_decimals);

--- user/utils.h
+++ user/utils.h
@@ -5,3 +5,5 @@
 #endif /* UTILS_H_ */
 
 ICACHE_FLASH_ATTR uint16_t crc_ccit_crc16(uint8_t *data_p, unsigned int length);
+ICACHE_FLASH_ATTR void float_to_string(float value, char *value_string, int8_t max_decimals);
+ICACHE_FLASH_ATTR float string_to_float(unsigned char *value_string, int8_t max_decimals);
