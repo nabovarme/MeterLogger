3b1551f147a281606baa0955d3c4d94f950ebc4b
st0ff3r <stoffer@skulp.net>
2016-06-12 02:32:18 +0200

    use os_get_random() to set IV

--- user/user_main.c
+++ user/user_main.c
@@ -752,16 +752,17 @@ ICACHE_FLASH_ATTR void system_init_done(void) {
 	
 	// ecc stuff
 	// NOTE:   String length must be evenly divisible by 16byte (str_len % 16 == 0)
-	//         You should pad the end of the string with zeros if this is not the case.
-	
+	//         You should pad the end of the string with zeros if this is not the case.	
 	uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
-	uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+	uint8_t iv[16];
 	uint8_t in[]  = "abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXZÆØÅ";
 	uint8_t buffer[64];
 	uint8_t in_orig[64];
 	uint8_t encrypted[64];
 	
 	memcpy(in_orig, in, sizeof(in_orig));
+	
+	os_get_random(iv, 16);
 	
 	AES128_CBC_encrypt_buffer(encrypted, in, sizeof(in), key, iv);
 	
