3e38dd15ee66c5de5922df4a61787204127f4d79
st0ff3r <stoffer@skulp.net>
2017-04-19 23:55:28 +0200

    even less mem for mqtt queue

--- include/user_config.h
+++ include/user_config.h
@@ -34,7 +34,7 @@
 #define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
 
 #ifdef AP
-#define QUEUE_BUFFER_SIZE		 		8192
+#define QUEUE_BUFFER_SIZE		 		4096
 #else
 #ifdef IMPULSE
 #define QUEUE_BUFFER_SIZE		 		20480		// larger queue for impulse meters
