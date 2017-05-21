f0b1b74dbdb0fcdcf3dd0cbd679f5440942b56d7
st0ff3r <stoffer@skulp.net>
2017-05-22 00:39:21 +0200

    send mqtt even if we did not got current time via ntp

--- user/en61107/en61107_request.c
+++ user/en61107/en61107_request.c
@@ -182,100 +182,99 @@ static void en61107_received_task(os_event_t *events) {
 
 			if (parse_mc66cde_standard_data_2_frame(&response, message, message_l)) {
 				current_unix_time = (uint32)(get_unix_time());		// TODO before 2038 ,-)
-				if (current_unix_time) {	// only send mqtt if we got current time via ntp
-   					// format /sample/v2/serial/unix_time => val1=23&val2=val3&baz=blah
-					memset(topic, 0, sizeof(topic));			// clear it
-					tfp_snprintf(current_unix_time_string, 64, "%u", (uint32_t)current_unix_time);
-					tfp_snprintf(topic, MQTT_TOPIC_L, "/sample/v2/%07u/%s", en61107_serial, current_unix_time_string);
+				// send mqtt even if we did not got current time via ntp
+   				// format /sample/v2/serial/unix_time => val1=23&val2=val3&baz=blah
+				memset(topic, 0, sizeof(topic));			// clear it
+				tfp_snprintf(current_unix_time_string, 64, "%u", (uint32_t)current_unix_time);
+				tfp_snprintf(topic, MQTT_TOPIC_L, "/sample/v2/%07u/%s", en61107_serial, current_unix_time_string);
 
-					memset(message, 0, sizeof(message));			// clear it
+				memset(message, 0, sizeof(message));			// clear it
 
-					// heap size
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "heap=%u&", system_get_free_heap_size());
-					strcat(message, key_value);
+				// heap size
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "heap=%u&", system_get_free_heap_size());
+				strcat(message, key_value);
 
-					// meter program
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "program=%01u%01u%03u%02u%01u%02u%02u&", 
-						response.meter_program.a, 
-						response.meter_program.b, 
-						response.meter_program.ccc, 
-						response.meter_program.dd, 
-						response.meter_program.e, 
-						response.meter_program.ff, 
-						response.meter_program.gg
-					);
-					strcat(message, key_value);
+				// meter program
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "program=%01u%01u%03u%02u%01u%02u%02u&", 
+					response.meter_program.a, 
+					response.meter_program.b, 
+					response.meter_program.ccc, 
+					response.meter_program.dd, 
+					response.meter_program.e, 
+					response.meter_program.ff, 
+					response.meter_program.gg
+				);
+				strcat(message, key_value);
 
-					// heating meter specific
-					// flow temperature
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "t1=%s %s&", response.t1.value, response.t1.unit);
-					strcat(message, key_value);
+				// heating meter specific
+				// flow temperature
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "t1=%s %s&", response.t1.value, response.t1.unit);
+				strcat(message, key_value);
         	
-					// return flow temperature
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "t2=%s %s&", response.t2.value, response.t2.unit);
-					strcat(message, key_value);
+				// return flow temperature
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "t2=%s %s&", response.t2.value, response.t2.unit);
+				strcat(message, key_value);
 
 #ifndef MC_66B
-					// t3 temperature
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "t3=%s %s&", response.t3.value, response.t3.unit);
-					strcat(message, key_value);
+				// t3 temperature
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "t3=%s %s&", response.t3.value, response.t3.unit);
+				strcat(message, key_value);
 #endif
 
-					// calculated temperature difference
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "tdif=%s %s&", response.tdif.value, response.tdif.unit);
-					strcat(message, key_value);
+				// calculated temperature difference
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "tdif=%s %s&", response.tdif.value, response.tdif.unit);
+				strcat(message, key_value);
 
-					// flow
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "flow1=%s %s&", response.flow1.value, response.flow1.unit);
-					strcat(message, key_value);
+				// flow
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "flow1=%s %s&", response.flow1.value, response.flow1.unit);
+				strcat(message, key_value);
 
-					// current power
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "effect1=%s %s&", response.effect1.value, response.effect1.unit);
-					strcat(message, key_value);
+				// current power
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "effect1=%s %s&", response.effect1.value, response.effect1.unit);
+				strcat(message, key_value);
 
-					// hours
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "hr=%s %s&", response.hr.value, response.hr.unit);
-					strcat(message, key_value);
+				// hours
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "hr=%s %s&", response.hr.value, response.hr.unit);
+				strcat(message, key_value);
 
-					// volume
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "v1=%s %s&", response.v1.value, response.v1.unit);
-					strcat(message, key_value);
+				// volume
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "v1=%s %s&", response.v1.value, response.v1.unit);
+				strcat(message, key_value);
 
-					// power
-					tfp_snprintf(key_value, MQTT_TOPIC_L, "e1=%s %s&", response.e1.value, response.e1.unit);
-					strcat(message, key_value);
+				// power
+				tfp_snprintf(key_value, MQTT_TOPIC_L, "e1=%s %s&", response.e1.value, response.e1.unit);
+				strcat(message, key_value);
 
-					memset(cleartext, 0, sizeof(cleartext));
-					os_strncpy(cleartext, message, sizeof(message));	// make a copy of message for later use
-					os_memset(message, 0, sizeof(message));				// ...and clear it
+				memset(cleartext, 0, sizeof(cleartext));
+				os_strncpy(cleartext, message, sizeof(message));	// make a copy of message for later use
+				os_memset(message, 0, sizeof(message));				// ...and clear it
 
-					// encrypt and send
-					message_l = encrypt_aes_hmac_combined(message, topic, strlen(topic), cleartext, strlen(cleartext) + 1);
+				// encrypt and send
+				message_l = encrypt_aes_hmac_combined(message, topic, strlen(topic), cleartext, strlen(cleartext) + 1);
 
-					// and stop retransmission timeout timer, last data from meter
-					os_timer_disarm(&en61107_receive_timeout_timer);
+				// and stop retransmission timeout timer, last data from meter
+				os_timer_disarm(&en61107_receive_timeout_timer);
 
-					// tell user_main we got a serial
-					if (en61107_meter_is_ready_cb) {
-						en61107_meter_is_ready_cb();
-					}
-#ifdef DEBUG
-					else {
-						os_printf("tried to call en61107_meter_is_ready_cb() before it was set - should not happen\n");
-					}
-#endif
-
-					if (mqtt_client) {
-						// if mqtt_client is initialized
-						if (message_l > 1) {
-							MQTT_Publish(mqtt_client, topic, message, message_l, 2, 0);	// QoS level 2
-						}
-					}
-
-					// change to last state - idle state
-					en61107_uart_state = UART_STATE_NONE;
-					en61107_request_num = 0;
+				// tell user_main we got a serial
+				if (en61107_meter_is_ready_cb) {
+					en61107_meter_is_ready_cb();
 				}
+#ifdef DEBUG
+				else {
+					os_printf("tried to call en61107_meter_is_ready_cb() before it was set - should not happen\n");
+				}
+#endif
+
+				if (mqtt_client) {
+					// if mqtt_client is initialized
+					if (message_l > 1) {
+						MQTT_Publish(mqtt_client, topic, message, message_l, 2, 0);	// QoS level 2
+					}
+				}
+
+				// change to last state - idle state
+				en61107_uart_state = UART_STATE_NONE;
+				en61107_request_num = 0;
 			}
 			break;
 	}
