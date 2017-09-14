b0a2f3861e376b6565ddb233546ccf4c3ac1223d
st0ff3r <stoffer@skulp.net>
2017-09-14 18:10:10 +0200

    show THERMO_ON_AC_2 in version string

--- user/version.h
+++ user/version.h
@@ -1,14 +1,30 @@
 #ifdef MC_66B
 #	ifdef THERMO_NO
-#		define HW_MODEL "MC-B-THERMO_NO"
+#		ifdef THERMO_ON_AC_2
+#			define HW_MODEL "MC-B-THERMO_NO-THERMO_ON_AC_2"
+#		else
+#			define HW_MODEL "MC-B-THERMO_NO"
+#		endif	// THERMO_ON_AC_2
 #	else	// THERMO_NC
-#		define HW_MODEL "MC-B-THERMO_NC"
+#		ifdef THERMO_ON_AC_2
+#			define HW_MODEL "MC-B-THERMO_NC-THERMO_ON_AC_2"
+#		else
+#			define HW_MODEL "MC-B-THERMO_NC"
+#		endif	// THERMO_ON_AC_2
 #	endif
 #elif defined(EN61107)
 #	ifdef THERMO_NO
-#		define HW_MODEL "MC-THERMO_NO"
+#		ifdef THERMO_ON_AC_2
+#			define HW_MODEL "MC-THERMO_NO-THERMO_ON_AC_2"
+#		else
+#			define HW_MODEL "MC-THERMO_NO"
+#		endif	// THERMO_ON_AC_2
 #	else	// THERMO_NC
-#		define HW_MODEL "MC-THERMO_NC"
+#		ifdef THERMO_ON_AC_2
+#			define HW_MODEL "MC-THERMO_NC-THERMO_ON_AC_2"
+#		else
+#			define HW_MODEL "MC-THERMO_NC"
+#		endif	// THERMO_ON_AC_2
 #	endif
 #elif defined(IMPULSE)
 #		define HW_MODEL "IMPULSE"
@@ -16,9 +32,17 @@
 #		define HW_MODEL "NO_METER"
 #else
 #	ifdef THERMO_NO
-#		define HW_MODEL "THERMO_NO"
+#		ifdef THERMO_ON_AC_2
+#			define HW_MODEL "THERMO_NO-THERMO_ON_AC_2"
+#		else
+#			define HW_MODEL "THERMO_NO"
+#		endif	// THERMO_ON_AC_2
 #	else	// THERMO_NC
-#		define HW_MODEL "THERMO_NC"
+#		ifdef THERMO_ON_AC_2
+#			define HW_MODEL "THERMO_NC-THERMO_ON_AC_2"
+#		else
+#			define HW_MODEL "THERMO_NC"
+#		endif	// THERMO_ON_AC_2
 #	endif
 #endif
 
