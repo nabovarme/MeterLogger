63f06fab93a9c1b483bcb104ae3e1578e854f167
st0ff3r <stoffer@skulp.net>
2018-03-15 02:33:29 +0100

    wrong placed #endif in utils.h

--- user/utils.h
+++ user/utils.h
@@ -1,9 +1,6 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
-
-#endif /* UTILS_H_ */
-
 ICACHE_FLASH_ATTR uint16_t ccit_crc16(uint16_t crc16, uint8_t *data_p, unsigned int length);
 ICACHE_FLASH_ATTR void w_to_kw_str(char *w, char *kw);
 ICACHE_FLASH_ATTR void kw_to_w_str(char *kw, char *w);
@@ -16,4 +13,6 @@ ICACHE_FLASH_ATTR unsigned int decimal_number_length(int n);
 ICACHE_FLASH_ATTR int int_pow(int x, int y);
 ICACHE_FLASH_ATTR int query_string_escape(char *str);
 ICACHE_FLASH_ATTR int query_string_unescape(char *str);
+
+#endif /* UTILS_H_ */
 
