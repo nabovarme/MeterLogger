e399b6df5c372a42a2089070cb9b69445f1f3c6f
st0ff3r <stoffer@skulp.net>
2018-04-04 16:53:45 +0200

    removed printf debug code

--- user/mqtt_rpc.c
+++ user/mqtt_rpc.c
@@ -454,13 +454,6 @@ void mqtt_rpc_reset_reason(MQTT_Client *client) {
 	struct rst_info *rtc_info;
 	rtc_info = system_get_rst_info();
 
-	if (rtc_info->reason == REASON_WDT_RST || rtc_info->reason == REASON_EXCEPTION_RST || rtc_info->reason == REASON_SOFT_WDT_RST) {
-		if (rtc_info->reason == REASON_EXCEPTION_RST) {
-			os_printf("Fatal exception (%d):\n", rtc_info->exccause);
-		}
-		os_printf("epc1=0x%08x, epc2=0x%08x, epc3=0x%08x, excvaddr=0x%08x, depc=0x%08x\n", rtc_info->epc1, rtc_info->epc2, rtc_info->epc3, rtc_info->excvaddr, rtc_info->depc);
-	}
-	//xxx
 #ifdef EN61107
 	tfp_snprintf(mqtt_topic, MQTT_TOPIC_L, "/reset_reason/v2/%07u/%u", en61107_get_received_serial(), get_unix_time());
 #elif defined IMPULSE
