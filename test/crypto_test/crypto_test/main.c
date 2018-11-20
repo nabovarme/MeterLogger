e2f36a1f4bc64c244c85b1bf28ff0a0ed7506757
Kristoffer Ek <stoffer@skulp.net>
2018-11-20 20:42:37 +0100

    updated xcode crypto_test

--- test/crypto_test/crypto_test/main.c
+++ test/crypto_test/crypto_test/main.c
@@ -12,7 +12,7 @@
 
 #include <esp8266.h>
 
-__attribute__((aligned(4))) uint8_t master_key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+__attribute__((aligned(4))) uint8_t master_key[] = { 0xef, 0x50, 0x0c, 0x92, 0x68, 0xcf, 0x74, 0x90, 0x16, 0xd2, 0x6d, 0x6c, 0xbf, 0xaa, 0xf7, 0xbf };
 
 uint8_t cleartext[] = "heap=21376&t1=23.61 C&t2=22.19 C&tdif=1.42 K&flow1=0 l/h&effect1=0.0 kW&hr=73327 h&v1=1321.27 m3&e1=56.726 MWh&";
 uint8_t topic[] = "/sample/v2/7210086/1466572820";
@@ -24,17 +24,37 @@ uint8_t buffer[MQTT_MESSAGE_L];
 int buffer_l;
 
 int main(int argc, const char * argv[]) {
+    unsigned int i;
     
     // init
     init_aes_hmac_combined(master_key);
     
     // encrypt
     mqtt_message_l = encrypt_aes_hmac_combined(mqtt_message, topic, strlen(topic) + 1, cleartext, strlen(cleartext) + 1);
+    printf("topic: %s\n", topic);
+    printf("cleartext message: %s\n", cleartext);
+    printf("IV: ");
+    for (i = 0; i < 16; i++) {
+        printf("%02x", (uint8_t)mqtt_message[SHA256_DIGEST_LENGTH + i]);
+    }
+    printf("\n");
+
+    printf("encrypted: ");
+    for (i = 0; i < (mqtt_message_l - SHA256_DIGEST_LENGTH - 16); i++) {
+        printf("%02x", (uint8_t)mqtt_message[SHA256_DIGEST_LENGTH + 16 + i]);
+    }
+    printf("\n");
+
+    printf("hmac sha256: ");
+    for (i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        printf("%02x", (uint8_t)dst[i]);
+    }
+    printf("\n");
 
     // decrypt
     memset(buffer, 0, sizeof(buffer));
     mqtt_message_l = decrypt_aes_hmac_combined(buffer, topic, strlen(topic) + 1, mqtt_message, mqtt_message_l);
-    printf("%s\n", buffer);
+    printf("\n%s\n", buffer);
 
     /*
     printf("----\n");
